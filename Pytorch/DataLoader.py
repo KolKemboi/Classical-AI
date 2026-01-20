48f9bd4ed9b46675601773892dfd3125e8b0ec53
KolKemboi <kolkemboi@gmail.com>
2026-01-20 09:21:35 +0300

    update

--- Pytorch/DataLoader.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-from matplotlib.pyplot import plt
-
-train_data = dataset.FashionMNIST
--- SEMANTIC SEGMENTATION/app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import torch
-import numpy as np
-from PIL import Image
-from  torchvision.transforms import transforms
-
